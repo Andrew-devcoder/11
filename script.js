9d706f8a85b0900cc829c5b19749384b9a21f5b4
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-05-12 15:43:54 +0300

    add scroll nav accent

--- script.js
+++ script.js
@@ -9,3 +9,45 @@ window.addEventListener("scroll", function () {
         navbar.classList.remove("shrink");
     }
 });
+
+const navLinks = document.querySelectorAll("nav a");
+
+function getPosition(element) {
+    let top = 0;
+
+    while (element) {
+        top += element.offsetTop;
+        element = element.offsetParent;
+    }
+
+    return top;
+}
+
+function getActiveSection() {
+    let fromTop = window.scrollY + 350;
+
+    for (let i = navLinks.length - 1; i >= 0; i--) {
+        let section = document.querySelector(navLinks[i].hash);
+        if (section) {
+            let position = getPosition(section);
+
+            if (
+                position <= fromTop &&
+                position + section.offsetHeight > fromTop
+            ) {
+                return navLinks[i];
+            }
+        }
+    }
+}
+
+window.addEventListener("scroll", () => {
+    let activeLink = getActiveSection();
+
+    if (activeLink) {
+        navLinks.forEach((link) => {
+            link.classList.remove("accent");
+        });
+        activeLink.classList.add("accent");
+    }
+});
